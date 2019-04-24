8f272488d0a4fa88c1e6084a037b54cad9f10fb8
Rohit <rohit.sharma9934@gmail.com>
2019-04-24 18:30:15 +0530

    Update amazon_reviews_nlp.py

--- amazon_reviews_nlp.py
+++ amazon_reviews_nlp.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import nltk
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import seaborn as sn
 import re
 import pickle 
